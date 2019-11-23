8a4b91d83e791ec550cc6c1678c8a5ea174e28cc
QuentinDaveau <daveau.quentin@hotmail.fr>
2019-11-23 14:09:13 +0100

    add green button

---
+++ Assets/Scripts/GreenButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenButton : Module
+{
+    public void ResetDone()
+    {
+        base.Enable();
+    }
+    public override void Enable()
+    {
+        animationPlayer.Play("GreenButtonReset");
+    }
+
+    public override void PointerClick()
+    {
+        animationPlayer.Play("GreenButtonClicked");
+        Activated();
+    }
+}
