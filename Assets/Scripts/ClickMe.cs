30d745d0c70af632348516c695eaca58892f8037
QuentinDaveau <daveau.quentin@hotmail.fr>
2019-11-23 18:22:26 +0100

    Add ClickMe

---
+++ Assets/Scripts/ClickMe.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMe : Module
+{
+    [SerializeField]
+    private float minDuration = 6f;
+
+    [SerializeField]
+    private float maxDuration = 10f;
+
+    private bool clickMe = false;
+    private bool pressed = false;
+
+    private Coroutine waitForBlink;
+
+
+
+    public void EnabledDone()
+    {
+        base.Enable();
+        waitForBlink = StartCoroutine(WaitBetweenBlink());
+    }
+
+    public override void Enable()
+    {
+        animationPlayer.Play("ClickEnable");
+    }
+
+    public void BlinkFinished()
+    {
+        clickMe = false;
+    }
+
+    public override void PointerClick()
+    {
+        animationPlayer.Play("ClickPress");
+        if(pressed) return;
+        if (!clickMe) return;
+
+        StopAllCoroutines();
+        Activated();
+    }
+
+    private IEnumerator WaitBetweenBlink()
+    {
+        yield return new WaitForSeconds(Random.Range(minDuration, maxDuration));
+        clickMe = true;
+        animationPlayer.Play("ClickBlink");
+    }
+}
